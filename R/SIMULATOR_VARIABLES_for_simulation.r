181f6b79841d916ff16a87e70802fe2bcf1bd10a
Dinh Ngoc Khanh <knd2127@columbia.edu>
2022-02-07 12:30:33 -0500

    Changes to make R program work

--- R/SIMULATOR_VARIABLES_for_simulation.r
+++ R/SIMULATOR_VARIABLES_for_simulation.r
@@ -4,6 +4,7 @@
 #' @return Nothing.
 #' @examples
 #' SIMULATOR_VARIABLES_for_simulation("FALLOPIAN-TUBES-NEUTRAL")
+#' @export
 #=============================SET ALL PARAMETERS REQUIRED FOR SIMULATION
 SIMULATOR_VARIABLES_for_simulation <- function(model) {
 #--------------------------------------Set up variables for cancer model
