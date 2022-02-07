045c9919b8f650a62989d0b6da3e63f64a196021
Dinh Ngoc Khanh <knd2127@columbia.edu>
2022-02-07 12:14:56 -0500

    Changes to make R program work

--- R/SIMULATOR_VARIABLES_for_simulation.r
+++ R/SIMULATOR_VARIABLES_for_simulation.r
@@ -1,8 +1,7 @@
 #' Add together two numbers
 #'
-#' @param x A number.
-#' @param y A number.
-#' @return The sum of \code{x} and \code{y}.
+#' @param model Name of model.
+#' @return Nothing.
 #' @examples
 #' add(1, 1)
 #' add(10, 1)
