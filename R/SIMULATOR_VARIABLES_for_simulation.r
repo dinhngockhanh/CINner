543ea88b6497ec9a3bf38383c9feccd773145028
Dinh Ngoc Khanh <knd2127@columbia.edu>
2022-02-07 12:09:08 -0500

    Changes to make R program work

--- R/SIMULATOR_VARIABLES_for_simulation.r
+++ R/SIMULATOR_VARIABLES_for_simulation.r
@@ -1,3 +1,11 @@
+#' Add together two numbers
+#'
+#' @param x A number.
+#' @param y A number.
+#' @return The sum of \code{x} and \code{y}.
+#' @examples
+#' add(1, 1)
+#' add(10, 1)
 #=============================SET ALL PARAMETERS REQUIRED FOR SIMULATION
 SIMULATOR_VARIABLES_for_simulation <- function(model) {
 #--------------------------------------Set up variables for cancer model
