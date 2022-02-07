f00c05e07c173233002009631a84d907fc2f9352
Dinh Ngoc Khanh <knd2127@columbia.edu>
2022-02-07 11:28:24 -0500

    Changes to make R program work

--- R/SIMULATOR_VARIABLES_for_simulation.r
+++ R/SIMULATOR_VARIABLES_for_simulation.r
@@ -1,6 +1,7 @@
 #=============================SET ALL PARAMETERS REQUIRED FOR SIMULATION
 SIMULATOR_VARIABLES_for_simulation <- function(model) {
 #--------------------------------------Set up variables for cancer model
+    
 #---Input table of variables from file
     filename                                <- paste(model,'-input-variables.csv',sep='')
     TABLE_VARIABLES                         <- read.table(filename,header=TRUE,sep=',')
