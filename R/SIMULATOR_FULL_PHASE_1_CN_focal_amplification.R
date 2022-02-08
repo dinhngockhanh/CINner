77b8715617ccd2ec1ad30c37ef01a12c3d20cda5
Dinh Ngoc Khanh <knd2127@columbia.edu>
2022-02-08 18:41:19 -0500

    Testing mechanism for tracking CN alleles information in R - make sure tracking is correct for focal amplification

--- R/SIMULATOR_FULL_PHASE_1_CN_focal_amplification.R
+++ R/SIMULATOR_FULL_PHASE_1_CN_focal_amplification.R
@@ -39,6 +39,14 @@ SIMULATOR_FULL_PHASE_1_CN_focal_amplification <- function(genotype_to_react,geno
         block_end       <- block_start+focal_length-1
         break
     }
+
+print('========================================BEFORE')
+print(ploidy_block[[chrom]][[strand]])
+print(ploidy_allele[[chrom]][[strand]])
+print('========================================INFORMATION')
+print(block_start)
+print(block_end)
+
 #   Find all drivers located on this region
     if ((driver_count==0)||(length(intersect(which(driver_map[,4]>=block_start),which(driver_map[,4]<=block_end)))==0)) {
         pos_drivers_to_amplify   <- c()
@@ -79,6 +87,11 @@ SIMULATOR_FULL_PHASE_1_CN_focal_amplification <- function(genotype_to_react,geno
         }
     }
 #------------------------------------------------Output the new genotype
+
+print('========================================AFTER')
+print(ploidy_block[[chrom]][[strand]])
+print(ploidy_allele[[chrom]][[strand]])
+
     genotype_list_ploidy_chrom[[genotype_daughter]]           <<- ploidy_chrom
     genotype_list_ploidy_allele[[genotype_daughter]]          <<- ploidy_allele
     genotype_list_ploidy_block[[genotype_daughter]]           <<- ploidy_block
