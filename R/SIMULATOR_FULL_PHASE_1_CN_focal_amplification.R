9d5f7a95452ac711864dd9516b96ad4deca4d0d4
Dinh Ngoc Khanh <knd2127@columbia.edu>
2022-02-08 17:46:33 -0500

    Correction for R

--- R/SIMULATOR_FULL_PHASE_1_CN_focal_amplification.R
+++ R/SIMULATOR_FULL_PHASE_1_CN_focal_amplification.R
@@ -57,6 +57,12 @@ SIMULATOR_FULL_PHASE_1_CN_focal_amplification <- function(genotype_to_react,geno
         # ploidy_allele[[chrom]][[strand]][block_CN+1:2*block_CN,block]   <- ploidy_allele[[chrom]][[strand]][1:block_CN,block]
 
         for(i_unit in block_CN+1:2*block_CN){
+print('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
+print(i_unit)
+print(nrow(ploidy_allele[[chrom]][[strand]]))
+print(block)
+print(ncol(ploidy_allele[[chrom]][[strand]]))
+
             ploidy_allele[[chrom]][[strand]][i_unit,block]              <- ploidy_allele[[chrom]][[strand]][i_unit-block_CN,block]
         }
     }
