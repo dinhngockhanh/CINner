13591253cbfe40b68f768351c2a6f02787421a1d
Dinh Ngoc Khanh <knd2127@columbia.edu>
2022-02-08 17:08:27 -0500

    Correction for R

--- R/SIMULATOR_FULL_PHASE_1_genotype_initiation.R
+++ R/SIMULATOR_FULL_PHASE_1_genotype_initiation.R
@@ -27,6 +27,9 @@ SIMULATOR_FULL_PHASE_1_genotype_initiation <- function(genotype_to_react) {
     genotype_list_DNA_length[[N_clones]]            <<- 0
     genotype_list_selection_rate[N_clones]          <<- 0
     genotype_list_prob_new_drivers[N_clones]        <<- 0
+
+print(genotype_list_ploidy_allele)
+
 #---------------------Create a new genotype for the second daughter cell
     N_clones                                        <<- N_clones+1
     genotype_daughter_2                             <- N_clones
@@ -45,7 +48,6 @@ SIMULATOR_FULL_PHASE_1_genotype_initiation <- function(genotype_to_react) {
         evolution_genotype_changes[[N_clones]]      <<- evolution_genotype_changes[[genotype_to_react]]
     }
 #---
-
     genotype_list_ploidy_chrom[[N_clones]]          <<- genotype_list_ploidy_chrom[[genotype_to_react]]
     genotype_list_ploidy_allele[[N_clones]]         <<- genotype_list_ploidy_allele[[genotype_to_react]]
     genotype_list_ploidy_block[[N_clones]]          <<- genotype_list_ploidy_block[[genotype_to_react]]
@@ -54,6 +56,9 @@ SIMULATOR_FULL_PHASE_1_genotype_initiation <- function(genotype_to_react) {
     genotype_list_DNA_length[[N_clones]]            <<- 0
     genotype_list_selection_rate[N_clones]          <<- 0
     genotype_list_prob_new_drivers[N_clones]        <<- 0
+
+print(genotype_list_ploidy_allele)
+
     output                                          <<- list()
     output[[1]]                                     <<- genotype_daughter_1
     output[[2]]                                     <<- genotype_daughter_2
