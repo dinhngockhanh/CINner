15cd8e6c4ae2eeecb33e3b11676fd77f59258f65
Dinh Ngoc Khanh <knd2127@columbia.edu>
2022-02-09 10:26:16 -0500

    Testing for R

--- R/SIMULATOR_FULL_PHASE_2_main.r
+++ R/SIMULATOR_FULL_PHASE_2_main.r
@@ -1,1 +1,26 @@
 #==============================================PHASE 2: SAMPLE PHYLOGENY
+SIMULATOR_FULL_PHASE_2_main <- function(package_clonal_evolution) {
+#---------------------------------------------Input the clonal evolution
+    T_current                       <- package_clonal_evolution[[1]]
+    N_clones                        <- package_clonal_evolution[[4]]
+
+
+
+    genotype_list_ploidy_chrom      <- package_clonal_evolution[[5]]
+    genotype_list_ploidy_block      <- package_clonal_evolution[[6]]
+    genotype_list_ploidy_allele     <- package_clonal_evolution[[7]]
+
+
+
+    evolution_traj_time             <- package_clonal_evolution[[13]]
+    evolution_traj_divisions        <- package_clonal_evolution[[14]]
+    evolution_traj_clonal_ID        <- package_clonal_evolution[[15]]
+    evolution_traj_population       <- package_clonal_evolution[[16]]
+
+
+
+print(T_current)
+print(N_clones)
+
+
+}
