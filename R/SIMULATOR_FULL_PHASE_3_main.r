98e82a7782aa5907ff6e34c6d192944abc0a936e
Dinh Ngoc Khanh <knd2127@columbia.edu>
2022-02-11 12:56:47 -0500

    Testing for R

--- R/SIMULATOR_FULL_PHASE_3_main.r
+++ R/SIMULATOR_FULL_PHASE_3_main.r
@@ -43,20 +43,129 @@ SIMULATOR_FULL_PHASE_3_main <- function(package_clonal_evolution,package_sample)
 #----------------------------------------Build the sample phylogeny tree
     for (i in seq(length(evolution_traj_divisions),1,-1)) {
 #       Get time point
-        time                        <- evolution_traj_time[i]
+        time                                    <- evolution_traj_time[i]
+#       Get current total clonal population (after divisions)
+        total_clonal_ID                         <- evolution_traj_clonal_ID[[i+1]]
+        total_clonal_population                 <- evolution_traj_population[[i+1]]
+#       Get current sample clonal population (after divisions)
+        sample_clonal_population                <- rep(0,length=N_clones)
+        for (node in 1:length(node_genotype_current)) {
+            genotype                            <- node_genotype_current[node]
+            sample_clonal_population[genotype]  <- sample_clonal_population[genotype]+1
+        }
+#       Get list of eligible nodes of each genotype
+        sample_eligible_nodes                   <- vector("list",length=N_clones)
+        for (node in 1:length(node_genotype_current)) {
+            genotype                            <- node_genotype_current[node]
+            sample_eligible_nodes[[genotype]]   <- c(sample_eligible_nodes[[genotype]], node_list_current[node])
+        }
+#       Get list of divisions
+        matrix_division                         <- evolution_traj_divisions[[i]]
+#       For each type of divisions...
+        for (event_type in 1:nrow(matrix_division)) {
+#           Get number of divisions
+            no_divisions                        <- matrix_division[event_type,1]
+#           Get genotype of mother
+            genotype_mother                     <- matrix_division[event_type,2]
+#           Get genotype of 1st daughter
+            genotype_daughter_1                 <- matrix_division[event_type,3]
+            position_daughter_1                 <- which(total_clonal_ID==genotype_daughter_1)
+#           Get genotype of 2nd daughter
+            genotype_daughter_2                 <- matrix_division[event_type,4]
+            position_daughter_2                 <- which(total_clonal_ID==genotype_daughter_2)
+#           If daughter genotypes are not in current nodes, move on
+            if ((sample_clonal_population[genotype_daughter_1]<=0)&&(sample_clonal_population[genotype_daughter_2]<=0)) {
+                next
+            }
+#           For each specific division...
+            for (division in 1:no_divisions) {
+#               If these genotypes are not in current nodes, move on
+                if ((sample_clonal_population[genotype_daughter_1]<=0)&&(sample_clonal_population[genotype_daughter_2]<=0)) {
+                    next
+                }
+#               Choose the first daughter node
+                logic_node_1                                                    <- runif(1)<sample_clonal_population[genotype_daughter_1]/total_clonal_population[position_daughter_1]
+                if (logic_node_1==1) {
+                    pos_node_1                                                  <- sample.int(sample_clonal_population[genotype_daughter_1],size=1)
+                    node_1                                                      <- sample_eligible_nodes[[genotype_daughter_1]][pos_node_1]
+                    sample_eligible_nodes[[genotype_daughter_1]][pos_node_1]    <- c()
+                    sample_clonal_population[genotype_daughter_1]               <- sample_clonal_population[genotype_daughter_1]-1
+                    total_clonal_population[position_daughter_1]                <- total_clonal_population[position_daughter_1]-1
+                }
+                else {
+                    node_1                                                      <- 0
+                    total_clonal_population[position_daughter_1]                <- total_clonal_population[position_daughter_1]-1
+                }
+#               Choose the second daughter node
+                logic_node_2                                                    <- runif(1)<sample_clonal_population[genotype_daughter_2]/total_clonal_population[position_daughter_2]
+                if (logic_node_2==1) {
+                    pos_node_2                                                  <- sample.int(sample_clonal_population[genotype_daughter_2],size=1)
+                    node_2                                                      <- sample_eligible_nodes[[genotype_daughter_2]][pos_node_2]
+                    sample_eligible_nodes[[genotype_daughter_2]][pos_node_2]    <- c()
+                    sample_clonal_population[genotype_daughter_2]               <- sample_clonal_population[genotype_daughter_2]-1
+                    total_clonal_population[position_daughter_2]                <- total_clonal_population[position_daughter_2]-1
+                    }
+                else {
+                    node_2                                                      <- 0
+                    total_clonal_population[position_daughter_2]                <- total_clonal_population[position_daughter_2]-1
+                }
+#               Update the nodes
+                if ((node_1==0)&&(node_2==0)) {
+#                   There is no merging....
+                    next
+                    }
+                else { if ((node_1>0)&&(node_2==0)) {
+#                   There is no merging but node 1 has one more division...
+#                   Update phylogeny in our style
+                    phylogeny_elapsed_gens[node_1]                          <- phylogeny_elapsed_gens[node_1]+1
+                    phylogeny_elapsed_genotypes[[node_1]]                   <- c(genotype_mother, phylogeny_elapsed_genotypes[[node_1]])
+#                   Update phylogeny records in our style
+                    node_genotype_current[which(node_list_current==node_1)] <- genotype_mother
+                    }
+                else { if ((node_1==0)&&(node_2>0)) {
+#                   There is no merging but node 2 has one more division...
+#                   Update phylogeny in our style
+                    phylogeny_elapsed_gens[node_2]                          <- phylogeny_elapsed_gens[node_2]+1
+                    phylogeny_elapsed_genotypes[[node_2]]                   <- c(genotype_mother, phylogeny_elapsed_genotypes[[node_2]])
+#                   Update phylogeny records in our style
+                    node_genotype_current[which(node_list_current==node_2)] <- genotype_mother
+                    }
+                else { if ((node_1>0)&&(node_2>0)) {
+#                   Nodes 1 and 2 are mergning...
+                    node_mother                                             <- min(node_list_current)-1
+#                   Update phylogeny in hclust style
 
-print(time)
 
 
 
 
 
-
-
-
+#                   Update phylogeny in our style
+                    phylogeny_origin[node_1]                                <- node_mother
+                    phylogeny_origin[node_2]                                <- node_mother
+                    phylogeny_elapsed_gens[node_mother]                     <- 1
+                    phylogeny_elapsed_genotypes[[node_mother]]              <- c(genotype_mother)
+                    phylogeny_genotype[node_mother]                         <- genotype_mother
+                    phylogeny_birthtime[node_1]                             <- time
+                    phylogeny_birthtime[node_2]                             <- time
+                    phylogeny_deathtime[node_mother]                        <- time
+#                   Update phylogeny records in our style
+                    pos_delete                                              <- c(which(node_list_current==node_1),which(node_list_current==node_2))
+                    node_genotype_current                                   <- node_genotype_current[-pos_delete]
+                    node_genotype_current                                   <- c(genotype_mother, node_genotype_current)
+                    node_list_current                                       <- node_list_current[-pos_delete]
+                    node_list_current                                       <- c(node_mother, node_list_current)
+                    }
+                }}}
+            }
+        }
     }
 
-
+print(phylogeny_origin)
+print(phylogeny_elapsed_gens)
+print(phylogeny_genotype)
+print(phylogeny_birthtime)
+print(phylogeny_deathtime)
 
 
 
