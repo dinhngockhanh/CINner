17fe631a1cbcc60a5b443b4d0edbc81152062575
Dinh Ngoc Khanh <knd2127@columbia.edu>
2022-02-16 09:16:20 -0500

    Testing for R

--- R/SIMULATOR_FULL_PHASE_1_selection_rate.r
+++ R/SIMULATOR_FULL_PHASE_1_selection_rate.r
@@ -27,7 +27,7 @@ SIMULATOR_FULL_PHASE_1_selection_rate <- function(driver_count,driver_map,ploidy
     }
 #----------If driver library is empty, then viable cells have sel rate 1
     if (nrow(driver_library)==0){
-        clone_selection_rate        <- 0
+        clone_selection_rate        <- 1
         return(clone_selection_rate)
     }
 #------------------------------Compute selection rates for viable clones
