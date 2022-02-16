70ebcfebaca9c34f25e2e13d7c676ba46e1bd049
Dinh Ngoc Khanh <knd2127@columbia.edu>
2022-02-16 09:36:29 -0500

    Testing for R

--- R/SIMULATOR_FULL_PROGRAM_many_simulations.r
+++ R/SIMULATOR_FULL_PROGRAM_many_simulations.r
@@ -1,7 +1,7 @@
 #================================================CREATE MANY SIMULATIONS
 SIMULATOR_FULL_PROGRAM_many_simulations <- function(model,stage_final,N_simulations) {
     for (i_simulation in 1:N_simulations){
-        print('+++++++++++++++++++++++++++++++ SIMULATION',i_simulation)
+        print(i_simulation)
         SIMULATOR_FULL_PROGRAM_one_simulation(model,stage_final)
     }
 }
