d0ee33f4f4ea15c549f45f47311afe0064287565
Dinh Ngoc Khanh <knd2127@columbia.edu>
2022-02-16 11:05:42 -0500

    Testing for R

--- R/SIMULATOR_FULL_PHASE_1_genotype_comparison.R
+++ R/SIMULATOR_FULL_PHASE_1_genotype_comparison.R
@@ -3,6 +3,7 @@ SIMULATOR_FULL_PHASE_1_genotype_comparison <- function(genotype_1,genotype_2) {
 #------------------------------------------Initialize flag of comparison
     output          <- 1
 #---------------------------------------Compare chromosome strand counts
+print('1')
     ploidy_chrom_1  <- genotype_list_ploidy_chrom[[genotype_1]]
     ploidy_chrom_2  <- genotype_list_ploidy_chrom[[genotype_2]]
     if (any(ploidy_chrom_1!=ploidy_chrom_2)) {
@@ -10,6 +11,7 @@ SIMULATOR_FULL_PHASE_1_genotype_comparison <- function(genotype_1,genotype_2) {
         return(output)
     }
 #-----------------------------Compare block CN on each chromosome strand
+print('2')
     ploidy_block_1  <- genotype_list_ploidy_block[[genotype_1]]
     ploidy_block_2  <- genotype_list_ploidy_block[[genotype_2]]
     for (chrom in 1:N_chromosomes) {
@@ -22,6 +24,7 @@ SIMULATOR_FULL_PHASE_1_genotype_comparison <- function(genotype_1,genotype_2) {
         }
     }
 #--------------------------------------Compare chromosome strand alleles
+print('3')
     ploidy_allele_1 <- genotype_list_ploidy_allele[[genotype_1]]
     ploidy_allele_2 <- genotype_list_ploidy_allele[[genotype_2]]
     for (chrom in 1:N_chromosomes) {
@@ -38,6 +41,7 @@ SIMULATOR_FULL_PHASE_1_genotype_comparison <- function(genotype_1,genotype_2) {
         }
     }
 #--------------------------------------------------Compare driver counts
+print('4')
     driver_count_1  <- genotype_list_driver_count[genotype_1]
     driver_count_2  <- genotype_list_driver_count[genotype_2]
     if (driver_count_1 != driver_count_2) {
@@ -45,6 +49,7 @@ SIMULATOR_FULL_PHASE_1_genotype_comparison <- function(genotype_1,genotype_2) {
         return(output)
     }
 #----------------------------------------------------Compare driver maps
+print('5')
     driver_map_1    <- genotype_list_driver_map[[genotype_1]]
     driver_map_2    <- genotype_list_driver_map[[genotype_2]]
     if ((nrow(driver_map_1) != nrow(driver_map_2)) || (ncol(driver_map_1) != ncol(driver_map_2))){
