ddce562bf165e2300f3d8109c9e2ce69bfe5f0fa
Dinh Ngoc Khanh <knd2127@columbia.edu>
2022-02-16 11:07:32 -0500

    Testing for R

--- R/SIMULATOR_FULL_PHASE_1_genotype_comparison.R
+++ R/SIMULATOR_FULL_PHASE_1_genotype_comparison.R
@@ -3,7 +3,6 @@ SIMULATOR_FULL_PHASE_1_genotype_comparison <- function(genotype_1,genotype_2) {
 #------------------------------------------Initialize flag of comparison
     output          <- 1
 #---------------------------------------Compare chromosome strand counts
-print('1')
     ploidy_chrom_1  <- genotype_list_ploidy_chrom[[genotype_1]]
     ploidy_chrom_2  <- genotype_list_ploidy_chrom[[genotype_2]]
     if (any(ploidy_chrom_1!=ploidy_chrom_2)) {
@@ -11,7 +10,6 @@ print('1')
         return(output)
     }
 #-----------------------------Compare block CN on each chromosome strand
-print('2')
     ploidy_block_1  <- genotype_list_ploidy_block[[genotype_1]]
     ploidy_block_2  <- genotype_list_ploidy_block[[genotype_2]]
     for (chrom in 1:N_chromosomes) {
@@ -24,7 +22,6 @@ print('2')
         }
     }
 #--------------------------------------Compare chromosome strand alleles
-print('3')
     ploidy_allele_1 <- genotype_list_ploidy_allele[[genotype_1]]
     ploidy_allele_2 <- genotype_list_ploidy_allele[[genotype_2]]
     for (chrom in 1:N_chromosomes) {
@@ -41,7 +38,6 @@ print('3')
         }
     }
 #--------------------------------------------------Compare driver counts
-print('4')
     driver_count_1  <- genotype_list_driver_count[genotype_1]
     driver_count_2  <- genotype_list_driver_count[genotype_2]
     if (driver_count_1 != driver_count_2) {
@@ -49,9 +45,17 @@ print('4')
         return(output)
     }
 #----------------------------------------------------Compare driver maps
-print('5')
     driver_map_1    <- genotype_list_driver_map[[genotype_1]]
     driver_map_2    <- genotype_list_driver_map[[genotype_2]]
+
+print('~~~~~~~~~~~~~~~~~~~~~')
+print(driver_map_1)
+print(driver_map_2)
+print(nrow(driver_map_1))
+print(nrow(driver_map_2))
+print(ncol(driver_map_1))
+print(ncol(driver_map_2))
+
     if ((nrow(driver_map_1) != nrow(driver_map_2)) || (ncol(driver_map_1) != ncol(driver_map_2))){
         output      <- 0
         return(output)
