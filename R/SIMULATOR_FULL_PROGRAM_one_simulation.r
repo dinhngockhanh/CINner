b39a86d6e8f4228d5299dcce4e48f66a52a7e081
Dinh Ngoc Khanh <knd2127@columbia.edu>
2022-02-19 10:18:15 -0500

    Testing for R

--- R/SIMULATOR_FULL_PROGRAM_one_simulation.r
+++ R/SIMULATOR_FULL_PROGRAM_one_simulation.r
@@ -5,9 +5,13 @@ SIMULATOR_FULL_PROGRAM_one_simulation <- function(model,stage_final) {
     flag_success                                    <- 0
     while (flag_success==0){
 print('CLONAL EVOLUTION')
+start.time  <- Sys.time()
         output                                      <- SIMULATOR_FULL_PHASE_1_main()
         flag_success                                <- output[[1]]
         package_clonal_evolution                    <- output[[2]]
+end.time    <- Sys.time()
+time.taken  <- end.time - start.time
+time.taken
     }
     if(stage_final>=1){
         package_output                              <- list()
@@ -16,14 +20,22 @@ print('CLONAL EVOLUTION')
 #----------------------------------------------------Simulate the sample
     if(stage_final>=2){
 print('SAMPLING')
+start.time  <- Sys.time()
         package_sample                              <- SIMULATOR_FULL_PHASE_2_main(package_clonal_evolution)
         package_output[[2]]                         <- package_sample
+end.time    <- Sys.time()
+time.taken  <- end.time - start.time
+time.taken
     }
 #-----------------------------------Simulate the phylogeny of the sample
     if(stage_final>=3){
 print('SAMPLE PHYLOGENY')
+start.time  <- Sys.time()
         package_sample_phylogeny                    <- SIMULATOR_FULL_PHASE_3_main(package_clonal_evolution,package_sample)
         package_output[[3]]                         <- package_sample_phylogeny
+end.time    <- Sys.time()
+time.taken  <- end.time - start.time
+time.taken
     }
 #------------------------------------------Output the simulation package
     return(package_output)
