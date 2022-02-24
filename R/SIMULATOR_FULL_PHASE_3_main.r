2cb4bcb02b7c9781a2eefd2edf343ff7e267c4a8
Dinh Ngoc Khanh <knd2127@columbia.edu>
2022-02-24 17:50:00 -0500

    Test

--- R/SIMULATOR_FULL_PHASE_3_main.r
+++ R/SIMULATOR_FULL_PHASE_3_main.r
@@ -347,6 +347,11 @@ SIMULATOR_FULL_PHASE_3_main <- function(package_clonal_evolution,package_sample)
         phylogeny_birthtime                                 <- phylogeny_birthtime[-list_unnecessary_nodes]
         phylogeny_deathtime                                 <- phylogeny_deathtime[-list_unnecessary_nodes]
     }
+
+
+print(phylogeny_origin)
+
+
 #-----------------------------------------Reorder the nodes for plotting
     list_roots                                      <- list_unmerged_nodes-N_unnecessary_nodes
 #---Find an order on all nodes of the phylogeny in our style
