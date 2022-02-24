e27c2743a8b0cd966ecdd65444ed54da3859d3a6
Dinh Ngoc Khanh <knd2127@columbia.edu>
2022-02-24 12:32:43 -0500

    Test

--- R/PLOT_copy_number_heatmap.r
+++ R/PLOT_copy_number_heatmap.r
@@ -9,10 +9,9 @@ PLOT_copy_number_heatmap <- function(package_simulation,phylo,plotcol,reorderclu
         phylogeny_clustering_truth    <- package_sample_phylogeny[[1]]
     }
 #--------------------------------------------Plot heatmap of CN profiles
-print(plotcol)
-    if (plotcol='total_copy'){
+    if (plotcol=='total_copy'){
         plotHeatmap(sample_genotype_profiles,plotcol="state",clusters=phylogeny_clustering_truth$clustering,tree=phylogeny_clustering_truth$tree,reorderclusters=TRUE,plottree=TRUE)
-    }else{if (plotcol='min_copy'){
-        plotHeatmap(sample_genotype_profiles,plotcol="Min",clusters=phylogeny_clustering_truth$clustering,tree=phylogeny_clustering_truth$tree,reorderclusters=TRUE,plottree=TRUE)        
+    }else{if (plotcol=='min_copy'){
+        plotHeatmap(sample_genotype_profiles,plotcol="Min",clusters=phylogeny_clustering_truth$clustering,tree=phylogeny_clustering_truth$tree,reorderclusters=TRUE,plottree=TRUE)
     }}
 }
