59df2593e75aa0bfce3bc1469f97bbb938bb99c7
Dinh Ngoc Khanh <knd2127@columbia.edu>
2022-02-24 12:15:58 -0500

    Test

--- R/PLOT_copy_number_heatmap.r
+++ R/PLOT_copy_number_heatmap.r
@@ -3,14 +3,20 @@ PLOT_copy_number_heatmap <- function(package_simulation,phylo,plotcol,reorderclu
 #------------------------Find phylogeny and clustering for sampled cells
     if (phylo=='true'){
         package_sample                <- package_simulation[[2]]
-        sample_genotype_profiles      <- package_sample[[1]]
         package_sample_phylogeny      <- package_simulation[[3]]
+
+        sample_genotype_profiles      <- package_sample[[1]]
         phylogeny_clustering_truth    <- package_sample_phylogeny[[1]]
     }
 #--------------------------------------------Plot heatmap of CN profiles
 
-print(reorderclusters)
-print(plottree)
+print(plotcol)
 
     plotHeatmap(sample_genotype_profiles,plotcol,clusters=phylogeny_clustering_truth$clustering,tree=phylogeny_clustering_truth$tree,reorderclusters,plottree)
 }
+
+
+
+# plotHeatmap(sample_genotype_profiles,plotcol="state",clusters=phylogeny_clustering_truth$clustering,tree=phylogeny_clustering_truth$tree,reorderclusters=TRUE,plottree=TRUE)
+
+# plotHeatmap(sample_genotype_profiles,plotcol="Min",clusters=phylogeny_clustering_truth$clustering,tree=phylogeny_clustering_truth$tree,reorderclusters=TRUE,plottree=TRUE)
