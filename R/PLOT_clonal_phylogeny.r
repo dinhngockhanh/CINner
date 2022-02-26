9e5c6124b5762bda0d8f498781ed83055b0c4f0e
Dinh Ngoc Khanh <knd2127@columbia.edu>
2022-02-26 17:56:01 -0500

    Test

--- R/PLOT_clonal_phylogeny.r
+++ R/PLOT_clonal_phylogeny.r
@@ -8,7 +8,7 @@ PLOT_clonal_phylogeny <- function(package_simulation){
 
     ape::plot.phylo(clone_phylogeny_phylo,direction="downward")
 
-    ape::edgelabels(text=TeX(r'(Using $\LaTeX$ for plotting in base graphics!)', bold=TRUE)),edge=i)
+    ape::edgelabels(text=TeX(r'(Using $\LaTeX$ for plotting in base graphics!)', bold=TRUE),edge=i)
 
     # ape::tiplabels()
 
