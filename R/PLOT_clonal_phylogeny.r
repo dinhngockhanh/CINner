c0781747b05030058715a978dc45b4265559d525
Dinh Ngoc Khanh <knd2127@columbia.edu>
2022-02-26 16:45:20 -0500

    Test

--- R/PLOT_clonal_phylogeny.r
+++ R/PLOT_clonal_phylogeny.r
@@ -3,7 +3,7 @@ PLOT_clonal_phylogeny <- function(package_simulation){
     package_sample_phylogeny        <- package_simulation[[3]]
     clone_phylogeny_phylo           <- package_sample_phylogeny[[2]]
 
-    i       <- which.edge(clone_phylogeny_phylo, c("A"))
+    i       <- ape::which.edge(clone_phylogeny_phylo, c("A"))
 
 
     ape::plot.phylo(clone_phylogeny_phylo)
