6fba42fcd809a71652ab9a2247597d00f6260c95
Dinh Ngoc Khanh <knd2127@columbia.edu>
2022-02-26 16:14:58 -0500

    Test

--- R/PLOT_clonal_phylogeny.r
+++ R/PLOT_clonal_phylogeny.r
@@ -3,5 +3,5 @@ PLOT_clonal_phylogeny <- function(package_simulation){
     package_sample_phylogeny        <- package_simulation[[3]]
     clone_phylogeny_phylo           <- package_sample_phylogeny[[2]]
 
-    plot(clone_phylogeny_phylo)
+    plot(clone_phylogeny_phylo,direction = "rightwards")
 }
