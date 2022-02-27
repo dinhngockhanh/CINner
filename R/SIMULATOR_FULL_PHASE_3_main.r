cfe89f38306138eea6c4f7448b5c53dc0f548420
Dinh Ngoc Khanh <knd2127@columbia.edu>
2022-02-27 16:32:30 -0500

    Test

--- R/SIMULATOR_FULL_PHASE_3_main.r
+++ R/SIMULATOR_FULL_PHASE_3_main.r
@@ -465,6 +465,9 @@ SIMULATOR_FULL_PHASE_3_main <- function(package_clonal_evolution,package_sample)
             genotype_mother                                         <- phylogeny_genotype[cell_node_mother]
             clone_node_mother                                       <- min(clone_node_list_current)-1
 #           Update clone phylogeny in hclust style
+
+print(clone_hclust_row)
+
             clone_hclust_row                                        <- clone_hclust_row+1
             clone_hclust_nodes[clone_node_mother]                   <- clone_hclust_row
             clone_hclust_merge[clone_hclust_row,]                   <- c(clone_hclust_nodes[clone_node_1],clone_hclust_nodes[clone_node_2])
@@ -501,7 +504,7 @@ SIMULATOR_FULL_PHASE_3_main <- function(package_clonal_evolution,package_sample)
 
                 if ((clone!=clone_node_mother) & (length(intersect(clone_phylogeny_elapsed_genotypes[[clone]],clone_phylogeny_elapsed_genotypes[[clone_node_mother]]))>0)){
 
-print(paste('EMERGENCY - CLONE ',clone,' AND CLONE ',clone_node_mother,sep=''))
+print(paste('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~EMERGENCY - CLONE ',clone,' AND CLONE ',clone_node_mother,sep=''))
 for (i in 1:length(clone_phylogeny_elapsed_genotypes)){
     print(paste(i,':   ',clone_phylogeny_elapsed_genotypes[[i]],sep=''))
 }
@@ -532,7 +535,7 @@ for (i in 1:length(clone_phylogeny_elapsed_genotypes)){
                     clone_node_list_current                                 <- clone_node_list_current[-pos_delete]
                     clone_node_list_current                                 <- c(clone_node_grandmother,clone_node_list_current)
 
-print('AFTER EMERGENCY:')
+print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~AFTER EMERGENCY:')
 for (i in 1:length(clone_phylogeny_elapsed_genotypes)){
 print(paste(i,':   ',clone_phylogeny_elapsed_genotypes[[i]],sep=''))
 }
