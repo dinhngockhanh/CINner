c10029b9bf780debebfb7ec7dc6efcae9791e62b
Dinh Ngoc Khanh <knd2127@columbia.edu>
2022-02-27 09:41:47 -0500

    Test

--- R/ABC_one_simulation.r
+++ R/ABC_one_simulation.r
@@ -5,10 +5,36 @@ ABC_one_simulation <- function(model,parameter_set) {
 #------------------------------------------Simulate the clonal evolution
     flag_success                                    <- 0
     while (flag_success==0) {
+print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~   CLONAL EVOLUTION')
+start.time  <- Sys.time()
         output                                      <- SIMULATOR_FULL_PHASE_1_main()
         flag_success                                <- output[[1]]
         package_clonal_evolution                    <- output[[2]]
+end.time    <- Sys.time()
+time.taken  <- end.time - start.time
+print(time.taken)
     }
+
+    package_output                                  <- list()
+    package_output[[1]]                             <- package_clonal_evolution
+
+print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~   SAMPLING')
+start.time  <- Sys.time()
+    package_sample                                  <- SIMULATOR_FULL_PHASE_2_main(package_clonal_evolution)
+    package_output[[2]]                             <- package_sample
+end.time    <- Sys.time()
+time.taken  <- end.time - start.time
+print(time.taken)
+
+print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~   SAMPLE PHYLOGENY')
+start.time  <- Sys.time()
+    package_sample_phylogeny                        <- SIMULATOR_FULL_PHASE_3_main(package_clonal_evolution,package_sample)
+    package_output[[3]]                             <- package_sample_phylogeny
+end.time    <- Sys.time()
+time.taken  <- end.time - start.time
+print(time.taken)
+
+print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~   DONE WITH SIMULATION')
 
 
 # print(package_clonal_evolution)
