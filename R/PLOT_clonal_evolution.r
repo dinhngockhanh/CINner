b7aaa7efae45fd927c13a7a996109479f1859a7a
Dinh Ngoc Khanh <knd2127@columbia.edu>
2022-02-28 10:13:48 -0500

    Test

--- R/PLOT_clonal_evolution.r
+++ R/PLOT_clonal_evolution.r
@@ -1,6 +1,13 @@
 #=====================================PLOT CLONAL EVOLUTION AS FISH PLOT
 PLOT_clonal_evolution <- function(package_simulation,vec_time_plot,unit){
-#------
+#---------------------------------------------Input the clonal phylogeny
+    package_sample_phylogeny        <- package_simulation[[3]]
+
+    clone_phylogeny_genotype        <- package_sample_phylogeny[[13]]
+
+
+    print(clone_phylogeny_genotype)
+
 
 print('PLOT CLONAL EVOLUTION ........')
 
