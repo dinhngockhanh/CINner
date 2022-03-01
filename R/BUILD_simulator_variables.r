f860cc68b58fb6a10820cdd5a2fbe967ca3d6d20
Dinh Ngoc Khanh <knd2127@columbia.edu>
2022-03-01 11:22:12 -0500

    Test

---
+++ R/BUILD_simulator_variables.r
@@ -0,0 +1,10 @@
+BUILD_simulator_variables <- function(cell_lifespan = 7,
+                                      T_0           = list(0,'year'),
+                                      T_end         = list(84,'year')){
+
+print(cell_lifespan)
+print(T_0)
+print(T_end)
+
+
+}
