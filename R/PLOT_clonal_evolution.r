171c08e526069747e90797fa1244b4d7cde09762
Dinh Ngoc Khanh <knd2127@columbia.edu>
2022-02-28 19:06:06 -0500

    Test

--- R/PLOT_clonal_evolution.r
+++ R/PLOT_clonal_evolution.r
@@ -116,12 +116,6 @@ PLOT_clonal_evolution <- function(package_simulation,vec_time_plot,unit){
         vec_row[vec_fix]                        <- 100
         table_clonal_populations[row,]          <- vec_row
     }
-
-
-    vec_time_plot               <- c(vec_time_plot[1]-1,vec_time_plot)
-    table_clonal_populations    <- cbind(rep(0,nrow(table_clonal_populations)),table_clonal_populations)
-
-
 #----------------------------------------------Plot the clonal evolution
     if (unit=='year'){
         vec_time_plot               <- vec_time_plot/365
