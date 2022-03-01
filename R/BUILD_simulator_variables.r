a202fd65f8a411e4dd66ff21a98ca64bd45584a5
Dinh Ngoc Khanh <knd2127@columbia.edu>
2022-03-01 16:07:55 -0500

    Test

--- R/BUILD_simulator_variables.r
+++ R/BUILD_simulator_variables.r
@@ -168,9 +168,7 @@ BUILD_general_variables <- function(model_name                           = 'MODE
     TABLE_POPULATION_DYNAMICS           <- data.frame(vec_time_points,vec_cell_count)
     colnames(TABLE_POPULATION_DYNAMICS) <- columns
 #----------------------------------------Output the model variable files
-    if (exists('MODEL_VARIABLES')==FALSE){
-        MODEL_VARIABLES                 <- list()
-    }
+    MODEL_VARIABLES                     <- list()
     MODEL_VARIABLES$general_variables   <- TABLE_VARIABLES
     MODEL_VARIABLES$cn_info             <- TABLE_CHROMOSOME_CN_INFO
     MODEL_VARIABLES$population_dynamics <- TABLE_POPULATION_DYNAMICS
@@ -181,41 +179,76 @@ BUILD_driver_library <- function(MODEL_VARIABLES    = list(),
                                  vec_driver_genes   = c(),
                                  vec_driver_role    = c(),
                                  vec_chromosome     = -1,
-                                 vec_bin            = -1){
+                                 vec_bin            = -1,
+                                 vec_driver_s       = c()){
 #-------------------------------------------Input the Cancer Gene Census
     DATA_cancer_gene_census             <- read.csv('../data/cancer_gene_census.csv')
 #-----------------------------------------------Build the driver library
-    columns                             <- c('Gene_ID','Gene_role')
-    TABLE_CANCER_GENES                  <- data.frame(vec_driver_genes,vec_driver_role)
+    columns                             <- c('Gene_ID','Gene_role','Selective_strength')
+    TABLE_CANCER_GENES                  <- data.frame(vec_driver_genes,vec_driver_role,vec_driver_s)
     colnames(TABLE_CANCER_GENES)        <- columns
-    if (length(vec_chromosome)<length(vec_driver_genes)){
-        TABLE_CANCER_GENES$Chromosome   <- 0
-    }
     if (any(vec_chromosome==-1)){
         TABLE_CANCER_GENES$Chromosome   <- -1
+    }else{
+        TABLE_CANCER_GENES$Chromosome   <- vec_chromosome
     }
     if (any(vec_bin==-1)){
         TABLE_CANCER_GENES$Bin          <- -1
-    }
-    if ((any(TABLE_CANCER_GENES$Chromosome==-1)==FALSE) & (any(TABLE_CANCER_GENES$Bin==-1)==FALSE)){
-        MODEL_VARIABLES$driver_library  <- TABLE_CANCER_GENES
-        return(MODEL_VARIABLES)
+    }else{
+        TABLE_CANCER_GENES$Bin          <- vec_bin
     }
 #--------------------------Supplement driver library with gene locations
-#   Get the CN bin length
-    size_CN_block_DNA                       <- as.numeric(MODEL_VARIABLES$general_variables$Value[MODEL_VARIABLES$general_variables$Variable=='size_CN_block_DNA'])
-#   Find the chromosome and bin for each driver
-    for (gene in 1:length(vec_driver_genes)){
-        Gene_ID                             <- vec_driver_genes[gene]
-        loc                                 <- which(DATA_cancer_gene_census$Gene.Symbol==Gene_ID)
-        Gene_address                        <- DATA_cancer_gene_census$Genome.Location[loc]
-        Gene_chromosome                     <- as.numeric(sub(':.*','',Gene_address))
-        Gene_bin                            <- round(as.numeric(sub('-.*','',sub('.*:','',Gene_address)))/size_CN_block_DNA)
-        TABLE_CANCER_GENES$Chromosome[gene] <- Gene_chromosome
-        TABLE_CANCER_GENES$Bin[gene]        <- Gene_bin
+    if ((any(TABLE_CANCER_GENES$Chromosome==-1)==TRUE) | (any(TABLE_CANCER_GENES$Bin==-1)==TRUE)){
+#       Get the CN bin length
+        size_CN_block_DNA                       <- as.numeric(MODEL_VARIABLES$general_variables$Value[MODEL_VARIABLES$general_variables$Variable=='size_CN_block_DNA'])
+#       Find the chromosome and bin for each driver
+        for (gene in 1:length(vec_driver_genes)){
+            Gene_ID                             <- vec_driver_genes[gene]
+            loc                                 <- which(DATA_cancer_gene_census$Gene.Symbol==Gene_ID)
+            Gene_address                        <- DATA_cancer_gene_census$Genome.Location[loc]
+            Gene_chromosome                     <- as.numeric(sub(':.*','',Gene_address))
+            Gene_bin                            <- round(as.numeric(sub('-.*','',sub('.*:','',Gene_address)))/size_CN_block_DNA)
+            TABLE_CANCER_GENES$Chromosome[gene] <- Gene_chromosome
+            TABLE_CANCER_GENES$Bin[gene]        <- Gene_bin
+        }
+    }
+#------------------Supplement driver library with allele selection rates
+
+
+
+#   Count the number of TSGs and ONCOGENEs
+    count_TSG                           <- sum(TABLE_CANCER_GENES$Gene_role=='TSG')
+    count_ONCOGENE                      <- sum(TABLE_CANCER_GENES$Gene_role=='ONCOGENE')
+#---Compute selection rates for TSGs
+    list_TSG                            <- which(TABLE_CANCER_GENES$Gene_role=='TSG')
+    s_normalization                     <- 1
+    for (driver in 1:length(list_TSG)){
+        row                             <- list_TSG[driver]
+#       Get its selection strength
+        driver_sel_rate                 <- vec_driver_s[row]
+#       Compute its selection rate for WT and MUT alleles
+        TABLE_CANCER_GENES$s_rate_WT[row]   <<- 1/(1+driver_sel_rate)
+        TABLE_CANCER_GENES$s_rate_MUT[row]  <<- 1
+#       Update normalizer for selection rate
+        s_normalization                 <- s_normalization*(1+driver_sel_rate)
+    }
+#---Compute selection rates for ONCOGENEs
+    s_normalization                     <- s_normalization^(1/count_ONCOGENE)
+    list_ONCOGENE                       <- which(TABLE_CANCER_GENES$Gene_role=='ONCOGENE')
+    for (driver in 1:length(list_ONCOGENE)){
+        row                             <- list_ONCOGENE[driver]
+#       Get its selection strength
+        driver_sel_rate                 <- vec_driver_s[row]
+#       Compute its selection rate for WT and MUT alleles
+        TABLE_CANCER_GENES$s_rate_WT[row]   <<- s_normalization
+        TABLE_CANCER_GENES$s_rate_MUT[row]  <<- s_normalization*(1+driver_sel_rate)
     }
 
-    MODEL_VARIABLES$driver_library      <- TABLE_CANCER_GENES
 
+
+
+
+#----------------------------------------Output the model variable files
+    MODEL_VARIABLES$TABLE_CANCER_GENES          <- TABLE_CANCER_GENES
     return(MODEL_VARIABLES)
 }
