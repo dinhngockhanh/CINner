e9a6d2e3a9e51784254bf5711e6139d866298beb
Dinh Ngoc Khanh <knd2127@columbia.edu>
2022-03-02 12:29:16 -0500

    Test

--- R/SIMULATOR_FULL_PHASE_1_selection_rate.r
+++ R/SIMULATOR_FULL_PHASE_1_selection_rate.r
@@ -40,8 +40,10 @@ SIMULATOR_FULL_PHASE_1_selection_rate <- function(driver_count,driver_map,ploidy
         block                                       <- driver_library_copy$Bin[i_driver]
         no_strands                                  <- ploidy_chrom[chrom]
         driver_copy                                 <- 0
-        for (strand in 1:no_strands){
-            driver_copy                             <- driver_copy+ploidy_block[[chrom]][[strand]][block]
+        if (no_strands>0){
+            for (strand in 1:no_strands){
+                driver_copy                         <- driver_copy+ploidy_block[[chrom]][[strand]][block]
+            }
         }
         driver_library_copy$Copy_WT[i_driver]       <- driver_copy
     }
