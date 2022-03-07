d8b625ff15a2954505a2eabebcfbdadb78944804
Dinh Ngoc Khanh <knd2127@columbia.edu>
2022-03-07 14:17:03 -0500

    Test

--- R/SAVE_cell_cn_profiles.r
+++ R/SAVE_cell_cn_profiles.r
@@ -1,5 +1,5 @@
 #====================================SAVE THE SAMPLED CELLS' CN PROFILES
-SAVE_cell_cn_profiles <- function(simulation,
+SAVE_cell_cn_profiles <- function(package_simulation,
                                     filename=''){
     package_sample                <- package_simulation[[2]]
     sample_genotype_profiles      <- package_sample[[1]]
