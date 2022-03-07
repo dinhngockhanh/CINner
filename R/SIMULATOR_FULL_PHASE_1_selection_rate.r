914ab065ab031951c0ea25555b278c1b53d47ae7
Dinh Ngoc Khanh <knd2127@columbia.edu>
2022-03-07 16:52:45 -0500

    Test

--- R/SIMULATOR_FULL_PHASE_1_selection_rate.r
+++ R/SIMULATOR_FULL_PHASE_1_selection_rate.r
@@ -1,7 +1,8 @@
 #==================================COMPUTE THE SELECTION RATE OF A CLONE
 SIMULATOR_FULL_PHASE_1_selection_rate <- function(driver_count,driver_map,ploidy_chrom,ploidy_block) {
 #-------------------------Cell is not viable if losing whole chromosomes
-#------------------------------------------or exceeding maximum CN count
+#------------------------------------or exceeding maximum average ploidy
+    vec_CN_all                      <- c()
     for (chrom in 1:N_chromosomes){
         no_strands                  <- ploidy_chrom[chrom]
         if (no_strands<=0){
@@ -9,18 +10,19 @@ SIMULATOR_FULL_PHASE_1_selection_rate <- function(driver_count,driver_map,ploidy
             return(clone_selection_rate)
         }
         vec_CN                      <- rep(0,vec_CN_block_no[chrom])
-
         for (strand in 1:no_strands){
             vec_CN                  <- vec_CN+ploidy_block[[chrom]][[strand]]
         }
-        if ((max(vec_CN)==0) || (max(vec_CN)>bound_ploidy)){
+        if (max(vec_CN)==0){
             clone_selection_rate    <- 0
             return(clone_selection_rate)
         }
+        vec_CN_all                  <- c(vec_CN_all,vec_CN)
     }
-
-    print(bound_ploidy)
-
+    if (mean(vec_CN_all)>bound_average_ploidy){
+        clone_selection_rate        <- 0
+        return(clone_selection_rate)
+    }
 #-------------------Cell is not viable if exceeding maximum driver count
     if (driver_count>0){
         driver_count_unique         <- unique(driver_map[,1])
