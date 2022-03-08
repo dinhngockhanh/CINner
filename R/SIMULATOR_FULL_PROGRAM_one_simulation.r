78f352c413fcf60611d4b8555001fa355f7d2ba0
Dinh Ngoc Khanh <knd2127@columbia.edu>
2022-03-08 11:47:35 -0500

    Test

--- R/SIMULATOR_FULL_PROGRAM_one_simulation.r
+++ R/SIMULATOR_FULL_PROGRAM_one_simulation.r
@@ -23,7 +23,7 @@ SIMULATOR_FULL_PROGRAM_one_simulation <- function(model='',
                                                                         print('');print('SAMPLING...');start.time  <- Sys.time()
             package_sample              <- SIMULATOR_FULL_PHASE_2_main(package_clonal_evolution)
                                                                         end.time<-Sys.time();time.taken<-end.time-start.time;print(time.taken)
-            N_clones                    <- length(package_sample[[4]])
+            N_clones                    <- nrow(output[[package_sample]])
             if ((N_clones<N_clones_min)|(N_clones>N_clones_max)){
                 flag_success            <- 0
             }
