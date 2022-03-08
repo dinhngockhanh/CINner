0ad86b3a405456e56b438e981253d65d363ade3f
Dinh Ngoc Khanh <knd2127@columbia.edu>
2022-03-08 18:12:44 -0500

    Test

--- R/BUILD_cn_normal_XX.r
+++ R/BUILD_cn_normal_XX.r
@@ -1,5 +1,6 @@
 BUILD_cn_normal_XX <- function(TABLE_CHROMOSOME_CN_INFO){
-
+#----------------------------------------------------Remove chromosome Y
+    TABLE_CHROMOSOME_CN_INFO    <- TABLE_CHROMOSOME_CN_INFO[-which(TABLE_CHROMOSOME_CN_INFO$Chromosome=='Y'),]
 
     print(TABLE_CHROMOSOME_CN_INFO)
 
