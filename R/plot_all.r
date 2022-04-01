8b4bfcf848fb1c4abc47eb6b2bc39f52d056b952
Dinh Ngoc Khanh <knd2127@columbia.edu>
2022-04-01 09:24:43 -0400

    Test

--- R/plot_all.r
+++ R/plot_all.r
@@ -8,52 +8,52 @@ plot_all <- function(model = "",
         width = 1000,
         height = 1000
     )
-    # #   Plot cell evolution as phylogeny tree
-    # cat("Plotting cell evolution as phylogeny tree...\n")
-    # plot_cell_phylo(
-    #     model = model,
-    #     n_simulations = n_simulations,
-    #     width = 1000,
-    #     height = 1000
-    # )
-    # #   Plot total population
-    # cat("Plotting total population vs input dynamics...\n")
-    # plot_tot_pop_vs_input(
-    #     model = model,
-    #     n_simulations = n_simulations,
-    #     vec_time = seq(0, 80, by = 1),
-    #     unit_time = "year",
-    #     width = 1000,
-    #     height = 500
-    # )
-    # #   Plot total CN profile
-    # cat("Plotting total CN profile...\n")
-    # plot_cn_heatmap(
-    #     model = model,
-    #     n_simulations = n_simulations,
-    #     plotcol = "total-copy",
-    #     phylo = TRUE,
-    #     width = 1000,
-    #     height = 1000
-    # )
-    # #   Plot minor CN profile
-    # cat("Plotting minor CN profile...\n")
-    # plot_cn_heatmap(
-    #     model = model,
-    #     n_simulations = n_simulations,
-    #     plotcol = "minor-copy",
-    #     phylo = TRUE,
-    #     width = 1000,
-    #     height = 1000
-    # )
-    # #   Plot clonal evolution as fish plot
-    # cat("Plotting clonal evolution as fish plot...\n")
-    # plot_clonal_fishplot(
-    #     model = model,
-    #     n_simulations = n_simulations,
-    #     vec_time = seq(0, 80, by = 1),
-    #     unit_time = "year",
-    #     width = 1000,
-    #     height = 500
-    # )
+    #   Plot cell evolution as phylogeny tree
+    cat("Plotting cell evolution as phylogeny tree...\n")
+    plot_cell_phylo(
+        model = model,
+        n_simulations = n_simulations,
+        width = 1000,
+        height = 1000
+    )
+    #   Plot total population
+    cat("Plotting total population vs input dynamics...\n")
+    plot_tot_pop_vs_input(
+        model = model,
+        n_simulations = n_simulations,
+        vec_time = seq(0, 80, by = 1),
+        unit_time = "year",
+        width = 1000,
+        height = 500
+    )
+    #   Plot total CN profile
+    cat("Plotting total CN profile...\n")
+    plot_cn_heatmap(
+        model = model,
+        n_simulations = n_simulations,
+        plotcol = "total-copy",
+        phylo = TRUE,
+        width = 1000,
+        height = 1000
+    )
+    #   Plot minor CN profile
+    cat("Plotting minor CN profile...\n")
+    plot_cn_heatmap(
+        model = model,
+        n_simulations = n_simulations,
+        plotcol = "minor-copy",
+        phylo = TRUE,
+        width = 1000,
+        height = 1000
+    )
+    #   Plot clonal evolution as fish plot
+    cat("Plotting clonal evolution as fish plot...\n")
+    plot_clonal_fishplot(
+        model = model,
+        n_simulations = n_simulations,
+        vec_time = seq(0, 80, by = 1),
+        unit_time = "year",
+        width = 1000,
+        height = 500
+    )
 }
