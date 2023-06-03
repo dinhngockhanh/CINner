780ba3b63cfc5818fc8e317b85bb93a6e523dfc9
Dinh Ngoc Khanh <knd2127@columbia.edu>
2023-06-03 08:48:59 -0400

    Fixes

--- R/plot_cn_heatmap.r
+++ R/plot_cn_heatmap.r
@@ -19,6 +19,133 @@ plot_cn_heatmap <- function(model = "",
                             n_cores = NULL,
                             R_libPaths = NULL) {
     library(signals)
+    plot_cn_heatmap_one_simulation <- function(model,
+                                               iteration,
+                                               folder_workplace,
+                                               folder_plots,
+                                               plotcol,
+                                               plottree,
+                                               plotfrequency,
+                                               show_legend,
+                                               show_library_label,
+                                               show_clone_label,
+                                               CN_data,
+                                               phylo,
+                                               filename_suffix,
+                                               width,
+                                               height) {
+        #------------------------------------------Input simulation file
+        filename <- paste(folder_workplace, model, "_simulation_", iteration, ".rda", sep = "")
+        load(filename)
+        #-----------------------------------------Decide filename suffix
+        if (is.null(filename_suffix)) {
+            if (CN_data == "TRUTH") {
+                filename_suffix <- "_cnTRUTH"
+            } else if (CN_data == "NEUTRAL-VARIATIONS") {
+                filename_suffix <- "_cnNEUVAR"
+            } else if (CN_data == "HMM") {
+                filename_suffix <- "_cnTRUTH+HMM"
+            } else if (CN_data == "NEUTRAL-VARIATIONS-HMM") {
+                filename_suffix <- "_cnNEUVAR+HMM"
+            }
+            if (phylo == "TRUTH") {
+                filename_suffix <- paste(filename_suffix, "_phyloTRUTH", sep = "")
+            } else if (phylo == "UMAP") {
+                filename_suffix <- paste(filename_suffix, "_phyloUMAP", sep = "")
+            }
+        }
+        #--------------------------------------------Extract CN profiles
+        if (CN_data == "TRUTH") {
+            #---Extract CN profiles from GROUND TRUTH
+            sample_genotype_profiles <- simulation$sample$cn_profiles_long
+            #   Remove internal nodes from true CN profiles
+            vec_delete <- c(
+                grep("Internal-node-", sample_genotype_profiles$cell_id, value = FALSE),
+                grep("Initial-clone-", sample_genotype_profiles$cell_id, value = FALSE)
+            )
+            if (length(vec_delete) > 0) {
+                sample_genotype_profiles <- sample_genotype_profiles[-vec_delete, ]
+            }
+        } else if (CN_data == "NEUTRAL-VARIATIONS") {
+            #---Extract CN profiles from GROUND TRUTH + NEUTRAL VARIATIONS
+            sample_genotype_profiles <- simulation$neutral_variations$sample$cn_profiles_long
+            #   Remove internal nodes from true CN profiles
+            vec_delete <- c(
+                grep("Internal-node-", sample_genotype_profiles$cell_id, value = FALSE),
+                grep("Initial-clone-", sample_genotype_profiles$cell_id, value = FALSE)
+            )
+            if (length(vec_delete) > 0) {
+                sample_genotype_profiles <- sample_genotype_profiles[-vec_delete, ]
+            }
+        } else if (CN_data == "HMM") {
+            #---Extract CN profiles from GROUND TRUTH + HMMcopy
+            sample_genotype_profiles <- simulation$sample$cn_profiles_long_hmm
+        } else if (CN_data == "NEUTRAL-VARIATIONS-HMM") {
+            #---Extract CN profiles from GROUND TRUTH + NEUTRAL VARIATIONS + HMMcopy
+            sample_genotype_profiles <- simulation$neutral_variations$sample$cn_profiles_long_hmm
+        }
+        sample_genotype_profiles$chr <- as.character(sample_genotype_profiles$chr)
+        #-------------------------------Extract clustering and phylogeny
+        if (phylo == "TRUTH") {
+            #---Extract clustering and phylogeny from GROUND TRUTH
+            sample_clustering <- simulation$sample_phylogeny$phylogeny_clustering_truth$clustering
+            sample_tree <- simulation$sample_phylogeny$phylogeny_clustering_truth$tree
+        } else if (phylo == "UMAP") {
+            #---Find clustering and phylogeny from UMAP
+            pctcells <- 0.05
+            umapmetric <- "euclidean"
+            seed <- NULL
+            ncells <- length(unique(sample_genotype_profiles$cell_id))
+            CNbins <- sample_genotype_profiles
+
+            clustering_results <- umap_clustering(CNbins,
+                minPts = max(round(pctcells * ncells), 2),
+                field = "copy",
+                umapmetric = umapmetric,
+                seed = seed
+            )
+            sample_clustering <- clustering_results$clustering %>% dplyr::select(cell_id, clone_id)
+            sample_tree <- clustering_results$tree
+        }
+        #-----------------------------------------Plot total CN profiles
+        if (plotcol == "total-copy") {
+            filename <- paste(folder_plots, model, "_sim", iteration, "_CN_total", filename_suffix, ".jpeg", sep = "")
+            jpeg(file = filename, width = width, height = height)
+            p <- plotHeatmap(sample_genotype_profiles,
+                plotcol = "state",
+                clusters = sample_clustering,
+                tree = sample_tree,
+                reorderclusters = TRUE,
+                plottree = plottree,
+                plotfrequency = plotfrequency,
+                show_legend = show_legend,
+                show_library_label = show_library_label,
+                show_clone_label = show_clone_label,
+                normalize_tree = FALSE,
+            )
+            print(p)
+            dev.off()
+        }
+        #-----------------------------------------Plot minor CN profiles
+        if (plotcol == "minor-copy") {
+            filename <- paste(folder_plots, model, "_sim", iteration, "_CN_minor", filename_suffix, ".jpeg", sep = "")
+            jpeg(file = filename, width = width, height = height)
+            p <- plotHeatmap(sample_genotype_profiles,
+                plotcol = "Min",
+                clusters = sample_clustering,
+                tree = sample_tree,
+                reorderclusters = TRUE,
+                plottree = plottree,
+                plotfrequency = plotfrequency,
+                show_legend = show_legend,
+                show_library_label = show_library_label,
+                show_clone_label = show_clone_label,
+                normalize_tree = FALSE
+            )
+            print(p)
+            dev.off()
+        }
+    }
     if (compute_parallel == FALSE) {
         #-----------------------------Plot CN heatmap in sequential mode
         for (iteration in 1:n_simulations) {
@@ -58,7 +185,7 @@ plot_cn_heatmap <- function(model = "",
         }
         clusterEvalQ(cl = cl, library(signals))
         #   Prepare input parameters for plotting
-        plot_cn_heatmap_one_simulation <<- plot_cn_heatmap_one_simulation
+        # plot_cn_heatmap_one_simulation <<- plot_cn_heatmap_one_simulation
         model <<- model
         folder_workplace <<- folder_workplace
         folder_plots <<- folder_plots
@@ -115,130 +242,130 @@ plot_cn_heatmap <- function(model = "",
     }
 }
 
-plot_cn_heatmap_one_simulation <- function(model,
-                                           iteration,
-                                           folder_workplace,
-                                           folder_plots,
-                                           plotcol,
-                                           plottree,
-                                           plotfrequency,
-                                           show_legend,
-                                           show_library_label,
-                                           show_clone_label,
-                                           CN_data,
-                                           phylo,
-                                           filename_suffix,
-                                           width,
-                                           height) {
-    #------------------------------------------Input simulation file
-    filename <- paste(folder_workplace, model, "_simulation_", iteration, ".rda", sep = "")
-    load(filename)
-    #-----------------------------------------Decide filename suffix
-    if (is.null(filename_suffix)) {
-        if (CN_data == "TRUTH") {
-            filename_suffix <- "_cnTRUTH"
-        } else if (CN_data == "NEUTRAL-VARIATIONS") {
-            filename_suffix <- "_cnNEUVAR"
-        } else if (CN_data == "HMM") {
-            filename_suffix <- "_cnTRUTH+HMM"
-        } else if (CN_data == "NEUTRAL-VARIATIONS-HMM") {
-            filename_suffix <- "_cnNEUVAR+HMM"
-        }
-        if (phylo == "TRUTH") {
-            filename_suffix <- paste(filename_suffix, "_phyloTRUTH", sep = "")
-        } else if (phylo == "UMAP") {
-            filename_suffix <- paste(filename_suffix, "_phyloUMAP", sep = "")
-        }
-    }
-    #--------------------------------------------Extract CN profiles
-    if (CN_data == "TRUTH") {
-        #---Extract CN profiles from GROUND TRUTH
-        sample_genotype_profiles <- simulation$sample$cn_profiles_long
-        #   Remove internal nodes from true CN profiles
-        vec_delete <- c(
-            grep("Internal-node-", sample_genotype_profiles$cell_id, value = FALSE),
-            grep("Initial-clone-", sample_genotype_profiles$cell_id, value = FALSE)
-        )
-        if (length(vec_delete) > 0) {
-            sample_genotype_profiles <- sample_genotype_profiles[-vec_delete, ]
-        }
-    } else if (CN_data == "NEUTRAL-VARIATIONS") {
-        #---Extract CN profiles from GROUND TRUTH + NEUTRAL VARIATIONS
-        sample_genotype_profiles <- simulation$neutral_variations$sample$cn_profiles_long
-        #   Remove internal nodes from true CN profiles
-        vec_delete <- c(
-            grep("Internal-node-", sample_genotype_profiles$cell_id, value = FALSE),
-            grep("Initial-clone-", sample_genotype_profiles$cell_id, value = FALSE)
-        )
-        if (length(vec_delete) > 0) {
-            sample_genotype_profiles <- sample_genotype_profiles[-vec_delete, ]
-        }
-    } else if (CN_data == "HMM") {
-        #---Extract CN profiles from GROUND TRUTH + HMMcopy
-        sample_genotype_profiles <- simulation$sample$cn_profiles_long_hmm
-    } else if (CN_data == "NEUTRAL-VARIATIONS-HMM") {
-        #---Extract CN profiles from GROUND TRUTH + NEUTRAL VARIATIONS + HMMcopy
-        sample_genotype_profiles <- simulation$neutral_variations$sample$cn_profiles_long_hmm
-    }
-    sample_genotype_profiles$chr <- as.character(sample_genotype_profiles$chr)
-    #-------------------------------Extract clustering and phylogeny
-    if (phylo == "TRUTH") {
-        #---Extract clustering and phylogeny from GROUND TRUTH
-        sample_clustering <- simulation$sample_phylogeny$phylogeny_clustering_truth$clustering
-        sample_tree <- simulation$sample_phylogeny$phylogeny_clustering_truth$tree
-    } else if (phylo == "UMAP") {
-        #---Find clustering and phylogeny from UMAP
-        pctcells <- 0.05
-        umapmetric <- "euclidean"
-        seed <- NULL
-        ncells <- length(unique(sample_genotype_profiles$cell_id))
-        CNbins <- sample_genotype_profiles
-
-        clustering_results <- umap_clustering(CNbins,
-            minPts = max(round(pctcells * ncells), 2),
-            field = "copy",
-            umapmetric = umapmetric,
-            seed = seed
-        )
-        sample_clustering <- clustering_results$clustering %>% dplyr::select(cell_id, clone_id)
-        sample_tree <- clustering_results$tree
-    }
-    #-----------------------------------------Plot total CN profiles
-    if (plotcol == "total-copy") {
-        filename <- paste(folder_plots, model, "_sim", iteration, "_CN_total", filename_suffix, ".jpeg", sep = "")
-        jpeg(file = filename, width = width, height = height)
-        p <- plotHeatmap(sample_genotype_profiles,
-            plotcol = "state",
-            clusters = sample_clustering,
-            tree = sample_tree,
-            reorderclusters = TRUE,
-            plottree = plottree,
-            plotfrequency = plotfrequency,
-            show_legend = show_legend,
-            show_library_label = show_library_label,
-            show_clone_label = show_clone_label,
-            normalize_tree = FALSE,
-        )
-        print(p)
-        dev.off()
-    }
-    #-----------------------------------------Plot minor CN profiles
-    if (plotcol == "minor-copy") {
-        filename <- paste(folder_plots, model, "_sim", iteration, "_CN_minor", filename_suffix, ".jpeg", sep = "")
-        jpeg(file = filename, width = width, height = height)
-        p <- plotHeatmap(sample_genotype_profiles,
-            plotcol = "Min",
-            clusters = sample_clustering,
-            tree = sample_tree,
-            reorderclusters = TRUE,
-            plottree = plottree,
-            plotfrequency = plotfrequency,
-            show_legend = show_legend,
-            show_library_label = show_library_label,
-            show_clone_label = show_clone_label,
-            normalize_tree = FALSE
-        )
-        print(p)
-        dev.off()
-    }
-}
+# plot_cn_heatmap_one_simulation <- function(model,
+#                                            iteration,
+#                                            folder_workplace,
+#                                            folder_plots,
+#                                            plotcol,
+#                                            plottree,
+#                                            plotfrequency,
+#                                            show_legend,
+#                                            show_library_label,
+#                                            show_clone_label,
+#                                            CN_data,
+#                                            phylo,
+#                                            filename_suffix,
+#                                            width,
+#                                            height) {
+#     #------------------------------------------Input simulation file
+#     filename <- paste(folder_workplace, model, "_simulation_", iteration, ".rda", sep = "")
+#     load(filename)
+#     #-----------------------------------------Decide filename suffix
+#     if (is.null(filename_suffix)) {
+#         if (CN_data == "TRUTH") {
+#             filename_suffix <- "_cnTRUTH"
+#         } else if (CN_data == "NEUTRAL-VARIATIONS") {
+#             filename_suffix <- "_cnNEUVAR"
+#         } else if (CN_data == "HMM") {
+#             filename_suffix <- "_cnTRUTH+HMM"
+#         } else if (CN_data == "NEUTRAL-VARIATIONS-HMM") {
+#             filename_suffix <- "_cnNEUVAR+HMM"
+#         }
+#         if (phylo == "TRUTH") {
+#             filename_suffix <- paste(filename_suffix, "_phyloTRUTH", sep = "")
+#         } else if (phylo == "UMAP") {
+#             filename_suffix <- paste(filename_suffix, "_phyloUMAP", sep = "")
+#         }
+#     }
+#     #--------------------------------------------Extract CN profiles
+#     if (CN_data == "TRUTH") {
+#         #---Extract CN profiles from GROUND TRUTH
+#         sample_genotype_profiles <- simulation$sample$cn_profiles_long
+#         #   Remove internal nodes from true CN profiles
+#         vec_delete <- c(
+#             grep("Internal-node-", sample_genotype_profiles$cell_id, value = FALSE),
+#             grep("Initial-clone-", sample_genotype_profiles$cell_id, value = FALSE)
+#         )
+#         if (length(vec_delete) > 0) {
+#             sample_genotype_profiles <- sample_genotype_profiles[-vec_delete, ]
+#         }
+#     } else if (CN_data == "NEUTRAL-VARIATIONS") {
+#         #---Extract CN profiles from GROUND TRUTH + NEUTRAL VARIATIONS
+#         sample_genotype_profiles <- simulation$neutral_variations$sample$cn_profiles_long
+#         #   Remove internal nodes from true CN profiles
+#         vec_delete <- c(
+#             grep("Internal-node-", sample_genotype_profiles$cell_id, value = FALSE),
+#             grep("Initial-clone-", sample_genotype_profiles$cell_id, value = FALSE)
+#         )
+#         if (length(vec_delete) > 0) {
+#             sample_genotype_profiles <- sample_genotype_profiles[-vec_delete, ]
+#         }
+#     } else if (CN_data == "HMM") {
+#         #---Extract CN profiles from GROUND TRUTH + HMMcopy
+#         sample_genotype_profiles <- simulation$sample$cn_profiles_long_hmm
+#     } else if (CN_data == "NEUTRAL-VARIATIONS-HMM") {
+#         #---Extract CN profiles from GROUND TRUTH + NEUTRAL VARIATIONS + HMMcopy
+#         sample_genotype_profiles <- simulation$neutral_variations$sample$cn_profiles_long_hmm
+#     }
+#     sample_genotype_profiles$chr <- as.character(sample_genotype_profiles$chr)
+#     #-------------------------------Extract clustering and phylogeny
+#     if (phylo == "TRUTH") {
+#         #---Extract clustering and phylogeny from GROUND TRUTH
+#         sample_clustering <- simulation$sample_phylogeny$phylogeny_clustering_truth$clustering
+#         sample_tree <- simulation$sample_phylogeny$phylogeny_clustering_truth$tree
+#     } else if (phylo == "UMAP") {
+#         #---Find clustering and phylogeny from UMAP
+#         pctcells <- 0.05
+#         umapmetric <- "euclidean"
+#         seed <- NULL
+#         ncells <- length(unique(sample_genotype_profiles$cell_id))
+#         CNbins <- sample_genotype_profiles
+#
+#         clustering_results <- umap_clustering(CNbins,
+#             minPts = max(round(pctcells * ncells), 2),
+#             field = "copy",
+#             umapmetric = umapmetric,
+#             seed = seed
+#         )
+#         sample_clustering <- clustering_results$clustering %>% dplyr::select(cell_id, clone_id)
+#         sample_tree <- clustering_results$tree
+#     }
+#     #-----------------------------------------Plot total CN profiles
+#     if (plotcol == "total-copy") {
+#         filename <- paste(folder_plots, model, "_sim", iteration, "_CN_total", filename_suffix, ".jpeg", sep = "")
+#         jpeg(file = filename, width = width, height = height)
+#         p <- plotHeatmap(sample_genotype_profiles,
+#             plotcol = "state",
+#             clusters = sample_clustering,
+#             tree = sample_tree,
+#             reorderclusters = TRUE,
+#             plottree = plottree,
+#             plotfrequency = plotfrequency,
+#             show_legend = show_legend,
+#             show_library_label = show_library_label,
+#             show_clone_label = show_clone_label,
+#             normalize_tree = FALSE,
+#         )
+#         print(p)
+#         dev.off()
+#     }
+#     #-----------------------------------------Plot minor CN profiles
+#     if (plotcol == "minor-copy") {
+#         filename <- paste(folder_plots, model, "_sim", iteration, "_CN_minor", filename_suffix, ".jpeg", sep = "")
+#         jpeg(file = filename, width = width, height = height)
+#         p <- plotHeatmap(sample_genotype_profiles,
+#             plotcol = "Min",
+#             clusters = sample_clustering,
+#             tree = sample_tree,
+#             reorderclusters = TRUE,
+#             plottree = plottree,
+#             plotfrequency = plotfrequency,
+#             show_legend = show_legend,
+#             show_library_label = show_library_label,
+#             show_clone_label = show_clone_label,
+#             normalize_tree = FALSE
+#         )
+#         print(p)
+#         dev.off()
+#     }
+# }
