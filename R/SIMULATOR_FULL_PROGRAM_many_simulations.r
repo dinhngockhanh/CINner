228388ec4e928c6dbb001e5963bb0827a1c24b7d
Dinh Ngoc Khanh <knd2127@columbia.edu>
2023-10-17 16:27:40 -0400

    Changes

--- R/SIMULATOR_FULL_PROGRAM_many_simulations.r
+++
@@ -1,8 +0,0 @@
-# ================================================CREATE MANY SIMULATIONS
-#' @export
-SIMULATOR_FULL_PROGRAM_many_simulations <- function(model, stage_final, N_simulations) {
-    for (i_simulation in 1:N_simulations) {
-        print(i_simulation)
-        SIMULATOR_FULL_PROGRAM_one_simulation(model, stage_final)
-    }
-}
